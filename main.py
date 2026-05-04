51616f49b0127dfe22245c5e28442744b89e6b39
gasser <183652335+gasse4@users.noreply.github.com>
2026-05-04 17:43:34 +0300

    Refactor ANN initialization to use dynamic input dimension and clean up whitespace

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ df = pd.read_csv("heart.csv")
 X = df.drop("target", axis=1)
 y = df["target"]
 
-
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.15, random_state=42
 )
@@ -21,7 +20,6 @@ X_train, X_test, y_train, y_test = train_test_split(
 #  Scaling
 
 scaler = StandardScaler()
-
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
@@ -35,11 +33,14 @@ y_train_t = torch.tensor(y_train.values, dtype=torch.float32).view(-1,1)
 X_test_t = torch.tensor(X_test, dtype=torch.float32)
 y_test_t = torch.tensor(y_test.values, dtype=torch.float32).view(-1,1)
 
+my_columns = X.shape[1]
+print  ("Number of features:", my_columns)
+
 class ANN(nn.Module):
-    def __init__(self):
+    def __init__(self , input_dim = my_columns):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Linear(13, 32),
+            nn.Linear(input_dim, 32),
             nn.ReLU(),
             nn.Linear(32, 16),
             nn.ReLU(),
@@ -73,8 +74,6 @@ with torch.no_grad():
     accuracy = (preds.eq(y_test_t).sum() / len(y_test_t)).item()
 
 print("Accuracy:", accuracy)
-
-
 
 sample = X_test_t[0].unsqueeze(0)
 
